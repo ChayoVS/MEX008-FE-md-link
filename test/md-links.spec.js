fd6696af5569402527f57c53c384d4aa5e6a4a0a
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-09-08 08:09:38 -0500

    Integrando proyecto, Agregué un par de test, quité los otros, agregue un par de archivos md para comprobar

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,24 +1,14 @@
-const mdLinks = require('../index.js');
+const mdLinks = require('../md-links.js');
 
-describe('mdLinks', () => {
-  it('tiene que ser una función', () => {
-      expect(typeof mdLinks).toBe('function');
-  });
+//estatus de un directorio
+test("mdLinks, deberia entregar un objeto con total: 7 y unique: 6 para la ruta test/mdTest con stats true",  () =>{
+   expect(mdLinks.mdLinks("test/mdTest",{stats:true})).resolves.toEqual({"total":7,"unique":6})
+});
+
+//estatus de un archivo
+test("mdLinks, deberia entregar un objeto con total: 3 y unique: 2 para el archivo fileTest2 con stats true",  () =>{
+  expect(mdLinks.mdLinks("test/mdTest/fileTest2.md",{stats:true})).resolves.toEqual({"total":3,"unique":2})
 });
 
 
 
-// Test para configurar JEST
-describe('initial', () => {
-  test('test inicial', () => {
-    expect(true).toBe(true)
-  })
-})
-
-// describe('mdLinks', () => {
-
-//   it('should...', () => {
-//     console.log('FIX ME!');
-//   });
-
-// });
---
+++ test/mdTest/fileTest1.md
@@ -0,0 +1,10 @@
+[Markdown](https://es.wikipedia.org/wiki/Markdown) 
+
+
+[Node.js](https://nodejs.org/en/)
+
+
+[npm](https://www.npmjs.com/)
+
+
+[EnlaceRoto](http://fdd.com/favicon.ico)
