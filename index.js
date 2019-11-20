8f3ad2380c31e378a276ea948760f562c74bd6dc
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-11-20 16:52:49 -0600

    Subiendo más funciones y cambios

--- index.js
+++ index.js
@@ -10,26 +10,26 @@ let options = {
 const figlet = require('figlet');
 
 
-// process.argv.forEach(element =>{
-//  if( element == "--stats"){
-//    options.stats = true
-//  }
-// if(element == "--validate"){
-//   options.validate = true
-// }
-// })
+process.argv.forEach(element =>{
+ if( element == "--stats"){
+   options.stats = true
+ }
+if(element == "--validate"){
+  options.validate = true
+}
+})
 
 mdLinks.mdLinks(path,options).then(res=>{
-  // if(options.validate && options.stats){
-  //   return console.log(chalk.magenta("Total Links: "+ res.total)+"\n"+chalk.green("Ok Links: "+res.ok)+"\n"+chalk.red("Broken Links: "+res.broken))
-  // }
-  // if(options.validate){
-  //   if(res.length === 0){
-  //     return console.log(chalk.red("No se encontraron links"))
-  //   }
-  //   let validateLinks = res.map(x=>x.file+"  "+chalk.blue(x.href)+"  "+chalk.cyan(x.text.substr(0,40))+"  "+x.status)
-  //   return console.log(validateLinks.join("\n "))
-  // }
+  if(options.validate && options.stats){
+    return console.log(chalk.magenta("Total Links: "+ res.total)+"\n"+chalk.green("Ok Links: "+res.ok)+"\n"+chalk.red("Broken Links: "+res.broken))
+  }
+  if(options.validate){
+    if(res.length === 0){
+      return console.log(chalk.red("No se encontraron links"))
+    }
+    let validateLinks = res.map(x=>x.file+"  "+chalk.blue(x.href)+"  "+chalk.cyan(x.text.substr(0,40))+"  "+x.status)
+    return console.log(validateLinks.join("\n "))
+  }
   if(options.stats){
     return console.log(chalk.magenta("Total Links: "+ res.total)+"\n"+chalk.yellow("Unique Links: "+res.unique))
   }else{
@@ -58,8 +58,6 @@ const iniciar = () => {
 const ejecutar = async () => {
   //Mostrar la información de la librería en la cabecera, título con figlet
   iniciar();
-
-  //Preguntas necesarias para validar archivo
  
 };
 ejecutar();
